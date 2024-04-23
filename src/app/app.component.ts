6b5cc0d93a3387f57ec4cc43f1b8afcb2a0c2db8
René Porzelt <rene.tube992@gmail.com>
2024-04-23 14:48:23 +0200

    change background global in shared component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StartScreenComponent } from './start-screen/start-screen.component';
+import { BackgroundComponent } from './shared/background/background.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StartScreenComponent],
+  imports: [RouterOutlet, BackgroundComponent, StartScreenComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
