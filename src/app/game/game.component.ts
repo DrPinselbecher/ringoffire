0d8b121433b4e7b6b2ceea5ad9885f76b0919630
René Porzelt <rene.tube992@gmail.com>
2024-05-01 16:02:10 +0200

    add game object and a shuffle function for cards

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,18 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrls: ['./game.component.scss']
 })
 export class GameComponent {
-
   pickCardAnimation = false;
+  game: Game;
+
+  constructor() {
+    this.game = new Game();
+    this.newGame();
+  }
 
   takeCard() {
     this.pickCardAnimation = true;
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
   }
 }
