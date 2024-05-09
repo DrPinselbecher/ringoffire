2d5b099c7c24447a395d94d4b7be604dbee48746
René Porzelt <rene.tube992@gmail.com>
2024-05-09 21:09:05 +0200

    added a component to turn the device if is in landscape mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { StartScreenComponent } from './start-screen/start-screen.component';
 import { BackgroundComponent } from './shared/background/background.component';
+import { StartScreenComponent } from './start-screen/start-screen.component';
+import { CommonModule } from '@angular/common';
+import { LandscapeMassageComponent } from './shared/landscape-massage/landscape-massage.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, BackgroundComponent, StartScreenComponent],
+  imports: [RouterOutlet, BackgroundComponent, StartScreenComponent, CommonModule, LandscapeMassageComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ringoffire';
+  isInLandscapeMode: boolean = false;
+
+  constructor() {
+    this.checkDimensions();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkDimensions();
+  }
 
   onDragStart(event: DragEvent) {
     event.preventDefault();
+  }
+
+  ngOnInit(): void {
+    this.checkDimensions();
+  }
+
+  private checkDimensions() {
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let userAgent = navigator.userAgent;
+    let isMobile = /iPhone|iPod|android.*mobile/i.test(userAgent);
+
+    this.isInLandscapeMode = width < 1000 && height < width && isMobile;
   }
 }
---
+++ src/app/shared/landscape-massage/landscape-massage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landscape-massage',
+  standalone: true,
+  imports: [],
+  templateUrl: './landscape-massage.component.html',
+  styleUrl: './landscape-massage.component.scss'
+})
+export class LandscapeMassageComponent {
+
+}
