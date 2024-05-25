18c570d2cc52bb868f52dab2983ad1004c76667a
René Porzelt <rene.tube992@gmail.com>
2024-05-25 19:35:05 +0200

    fix the restarting bug

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -173,6 +173,7 @@ export class GameComponent {
     this.game.playedCards = [];
     this.game.setCards();
     this.game.currentCard = undefined;
+    this.gameService.updateGame();
   }
 
   isSafariBrowserOnSmartphone() {
